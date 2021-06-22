b494408bd8613739615400777c06f8a996faadb1
Airam Miranda <airam1968@terra.com.br>
2021-06-22 07:58:31 -0300

    Atualização 22062021

---
+++ src/Avanade.SubTCSE.Projeto.Domain/Aggregates/EmployeeRole/Entities/EmployeeRole.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Avanade.SubTCSE.Projeto.Domain.Aggregates.EmployeeRole.Entities
+{
+    public record EmployeeRole(string id, string roleName)
+    {
+
+        public string Id { get; private set; } //Princípio da imutabilidade com private set, um construtor ainda pode modificar
+
+        public string RoleName { get; init; } //Princípio da imutabilidade com init
+    }
+}
